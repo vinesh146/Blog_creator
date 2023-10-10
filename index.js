c34ef1126070b50d09bf995878f095f76a76581b
vinesh146 <136746912+vinesh146@users.noreply.github.com>
2023-10-10 12:06:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import  {  All } from './App';
-import "./index.css";
+import "./styles.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
